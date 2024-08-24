d024a21cc1394d7f82325838118014eba06a0fb6
AriePriyambadha <6871368+AriePriyambadha@users.noreply.github.com>
2024-08-24 18:55:17 +0700

    Reposition our function to better readability and limit size to 100

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
+function changeRandomColor() {
+  const boxes = document.querySelectorAll("div.smallBox");
+
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", (event) => {
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random() * 256);
+      const rgb = `rgb(${red}, ${green}, ${blue})`;
+
+      event.target.style.setProperty("background-color", rgb);
+    });
+  });
+}
+
 function createGrid(size) {
   const container = document.querySelector("#container");
 
+  // Set the limit grid size up to 100.
+  if (size > 100) size = 100;
   for (let i = 0; i < size; i++) {
     container.append(document.createElement("div"));
   }
@@ -20,33 +37,20 @@ function createGrid(size) {
   changeRandomColor();
 }
 
-function changeRandomColor() {
-  const boxes = document.querySelectorAll("div.smallBox");
-
-  boxes.forEach((box) => {
-    box.addEventListener("mouseover", (event) => {
-      const red = Math.floor(Math.random() * 256);
-      const green = Math.floor(Math.random() * 256);
-      const blue = Math.floor(Math.random() * 256);
-      const rgb = `rgb(${red}, ${green}, ${blue})`;
-
-      event.target.style.setProperty("background-color", rgb);
-    });
-  });
-}
-
+// Event occur when the page load compleatly (start page)
 document.addEventListener("DOMContentLoaded", createGrid(16));
 
 const button = document.querySelector("button");
 const size = document.querySelector("#size");
 
+// Event occur when the user hover mouse to the area
 button.addEventListener("click", () => {
   const size = document.querySelector("#size");
   const container = document.getElementById("container");
   const body = document.querySelector("body");
 
-  console.log(container);
-
+  // Make sure if the container already exist or not,
+  // if exist we need to delete and try to create another one !!
   if (container) {
     container.remove();
 
