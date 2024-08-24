1bdeed46a4a58a58102b4054ebebe3f5b6f23cfb
AriePriyambadha <6871368+AriePriyambadha@users.noreply.github.com>
2024-08-24 18:26:04 +0700

    Create function to random color when the mouse hover to the target area
    We also remove border in the middle of container, but still keep border
    container alive.

--- scripts.js
+++ scripts.js
@@ -16,6 +16,23 @@ function createGrid(size) {
       grid.appendChild(smallBox);
     }
   });
+
+  changeRandomColor();
+}
+
+function changeRandomColor() {
+  const boxes = document.querySelectorAll("div.smallBox");
+
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", (event) => {
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random() * 256);
+      const rgb = `rgb(${red}, ${green}, ${blue})`;
+
+      event.target.style.setProperty("background-color", rgb);
+    });
+  });
 }
 
 document.addEventListener("DOMContentLoaded", createGrid(16));
@@ -23,7 +40,7 @@ document.addEventListener("DOMContentLoaded", createGrid(16));
 const button = document.querySelector("button");
 const size = document.querySelector("#size");
 
-button.addEventListener("click", (event) => {
+button.addEventListener("click", () => {
   const size = document.querySelector("#size");
   const container = document.getElementById("container");
   const body = document.querySelector("body");
